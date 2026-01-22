28a01ad5c4cb50f7845909e05584e4fa7364d910
Alex Sostin <AlexSostin2108@gmail.com>
2026-01-22 10:14:32 +0200

    Fix images: add Cloudinary and YouTube to remotePatterns

--- frontend-next/next.config.ts
+++ frontend-next/next.config.ts
@@ -30,6 +30,21 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'images.unsplash.com',
       },
     ],
